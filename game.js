b9bcbead44c15bffe4b3b18f0ee1ce14d664734a
VV-WIN <51390159+VV-WIN@users.noreply.github.com>
2024-04-03 13:50:23 -0700

    Add key state reference

---
+++ game.js
@@ -0,0 +1,13 @@
+/* Direction key state */
+ const directions = {
+    up: "up",
+    down: "down",
+    left: "left",
+    right: "right",
+ }
+ const keys = {
+    38: directions.up,
+    37: directions.left,
+    39: directions.right,
+    40: directions.down,
+ }
