85a6d72247581a62b75e03609fa10fb7d18ce613
VV-WIN <51390159+VV-WIN@users.noreply.github.com>
2024-04-05 10:32:58 -0700

    Refactor js

--- game.js
+++ game.js
@@ -1,56 +1,3 @@
-var character = document.querySelector(".character");
-var map = document.querySelector(".map");
-
-//start in the middle of the map
-var x = 90;
-var y = 34;
-var held_directions = []; //State of which arrow keys we are holding down
-var speed = 1; //How fast the character moves in pixels per frame
-
-const placeCharacter = () => {
-
-   var pixelSize = parseInt(
-      getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
-   );
-
-   const held_direction = held_directions[0];
-   if (held_direction) {
-      if (held_direction === directions.right) { x += speed; }
-      if (held_direction === directions.left) { x -= speed; }
-      if (held_direction === directions.down) { y += speed; }
-      if (held_direction === directions.up) { y -= speed; }
-      character.setAttribute("facing", held_direction);
-   }
-   character.setAttribute("walking", held_direction ? "true" : "false");
-
-   //Limits (gives the illusion of walls)
-   var leftLimit = -8;
-   var rightLimit = (16 * 11) + 8;
-   var topLimit = -8 + 32;
-   var bottomLimit = (16 * 7);
-   if (x < leftLimit) { x = leftLimit; }
-   if (x > rightLimit) { x = rightLimit; }
-   if (y < topLimit) { y = topLimit; }
-   if (y > bottomLimit) { y = bottomLimit; }
-
-
-   var camera_left = pixelSize * 66;
-   var camera_top = pixelSize * 42;
-
-   map.style.transform = `translate3d( ${-x * pixelSize + camera_left}px, ${-y * pixelSize + camera_top}px, 0 )`;
-   character.style.transform = `translate3d( ${x * pixelSize}px, ${y * pixelSize}px, 0 )`;
-}
-
-//Set up the game loop
-const step = () => {
-   placeCharacter();
-   window.requestAnimationFrame(() => {
-      step();
-   })
-}
-step();
-
-/* Direction key state */
 const directions = {
    up: "up",
    down: "down",
@@ -58,22 +5,91 @@ const directions = {
    right: "right",
 }
 const keys = {
-   38: directions.up,
-   37: directions.left,
-   39: directions.right,
-   40: directions.down,
+   'ArrowUp': directions.up,
+   'ArrowLeft': directions.left,
+   'ArrowRight': directions.right,
+   'ArrowDown': directions.down,
 }
+
+const character = document.querySelector(".character");
+const map = document.querySelector(".map");
+
+//start in the middle of the map
+let x = 90;
+let y = 34;
+let pressedDirections = []; //State of which arrow keys we are holding down
+const speed = 1; //How fast the character moves in pixels per frame
+
+const placeCharacter = () => {
+   const pixelSize = parseInt(
+       getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
+   );
+
+   const direction = pressedDirections[0];
+   if (direction) {
+      if (direction === directions.right) {x += speed;}
+      if (direction === directions.left) {x -= speed;}
+      if (direction === directions.down) {y += speed;}
+      if (direction === directions.up) {y -= speed;}
+      character.setAttribute("facing", direction);
+   }
+   character.setAttribute("walking", direction ? "true" : "false");
+
+   //Limits (gives the illusion of walls)
+   const leftLimit = -8;
+   const rightLimit = (16 * 11)+8;
+   const topLimit = -8 + 32;
+   const bottomLimit = (16 * 7);
+   if (x < leftLimit) { x = leftLimit; }
+   if (x > rightLimit) { x = rightLimit; }
+   if (y < topLimit) { y = topLimit; }
+   if (y > bottomLimit) { y = bottomLimit; }
+
+
+   const CAMERA_LEFT_OFFSET_PX = 66;
+   const CAMERA_TOP_OFFSET_PX = 42;
+
+   //Update camera
+   const camera_transform_left = -x*pixelSize+(pixelSize * CAMERA_LEFT_OFFSET_PX);
+   const camera_transform_top = -y*pixelSize+(pixelSize * CAMERA_TOP_OFFSET_PX);
+   map.style.transform = `translate3d( ${camera_transform_left}px, ${camera_transform_top}px, 0 )`;
+
+   //Update character
+   character.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0 )`;
+}
+
+//Set up the game loop
+let previousMs;
+const stepTime = 1 / 60;
+const tick = (timestampMs) => {
+   if (previousMs === undefined) {
+      previousMs = timestampMs;
+   }
+   let delta = (timestampMs - previousMs) / 1000;
+   while (delta >= stepTime) {
+      placeCharacter();
+      delta -= stepTime;
+   }
+   previousMs = timestampMs - delta * 1000; // Make sure we don't lose unprocessed (delta) time
+
+   //Recapture the callback to be able to shut it off
+   requestAnimationFrame(tick);
+}
+requestAnimationFrame(tick); //kick off the first step!
+
+
+/* Direction key state */
 document.addEventListener("keydown", (e) => {
-   var dir = keys[e.which];
-   if (dir && held_directions.indexOf(dir) === -1) {
-      held_directions.unshift(dir)
+   const dir = keys[e.code];
+   if (dir && pressedDirections.indexOf(dir) === -1) {
+      pressedDirections.unshift(dir)
    }
 })
 
 document.addEventListener("keyup", (e) => {
-   var dir = keys[e.which];
-   var index = held_directions.indexOf(dir);
+   const dir = keys[e.code];
+   const index = pressedDirections.indexOf(dir);
    if (index > -1) {
-      held_directions.splice(index, 1)
+      pressedDirections.splice(index, 1)
    }
 });
